b9e02f530e314e31fa9916811d765314fae5fbaa
inuzuki1113 <daweiha413@gmail.com>
2025-09-14 02:26:17 +0900

    Create video.c

---
+++ source/video.c
@@ -0,0 +1,15 @@
+#include "video.h"
+#include <3ds.h>
+
+void videoInit() {
+    // 上画面初期化
+}
+
+void videoExit() {
+    // 上画面終了処理
+}
+
+void updateVideo() {
+    // 現在選択中の動画を144pで再生
+    // CTRMP4形式を読み込み
+}
